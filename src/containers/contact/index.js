f93dab5673effdfd984de44a8eea8cec45591c72
VENKATESANSK <venkatesan.sk@gmail.com>
2018-03-28 11:49:58 +0530

    first commit

---
+++ src/containers/contact/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+//require( 'datatables.net-dt' )();ss
+const Contact = () => (
+  <div>
+    <h1>contact Page</h1>
+    <p>Did you get here via Redux?</p>
+  </div>
+);
+
+export default Contact;
